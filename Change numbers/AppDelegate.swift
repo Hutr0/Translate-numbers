f6b33e7e03e1e8f04f9fa3ada958eba611daa230
Hutr0 <Hutro-games@yandex.ru>
2021-02-17 15:26:43 +0300

    Initial Commit

---
+++ Change numbers/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Change numbers
+//
+//  Created by Леонид Лукашевич on 17.02.2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Change numbers/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Change numbers
+//
+//  Created by Леонид Лукашевич on 17.02.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
